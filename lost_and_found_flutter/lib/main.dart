3f781b672c16233014bf55dbbd9d8ae5a80234a9
Muhammad Abdullah <abdullahimtiazyousafzai@gmail.com>
2025-03-13 14:30:00 +0500

    Week 2

---
+++ lost_and_found_flutter/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'auth/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KHOJ - Talash-e-Gumshuda',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(), // <-- Set your custom login page here
+    );
+  }
+}
